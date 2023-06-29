6cd441b43d4a5873296fa05a820db6f58a5ec9bd
Alexpdsantos <amelo3433@gmail.com>
2023-06-28 22:32:36 -0300

    Adicionados models de Produto, variações, pedido, itempedido

--- loja/settings.py
+++ loja/settings.py
@@ -34,6 +34,8 @@ ALLOWED_HOSTS = ['127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'produto',
+    'pedido',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
