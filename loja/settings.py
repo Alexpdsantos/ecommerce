7ad1ed285756e6f7fc769867dbce7e13e9bee316
Alexpdsantos <amelo3433@gmail.com>
2023-07-09 19:56:34 -0300

    Criação do model Perfil e alterações em Produto

--- loja/settings.py
+++ loja/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['127.0.0.1']
 INSTALLED_APPS = [
     'produto',
     'pedido',
+    'perfil',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
